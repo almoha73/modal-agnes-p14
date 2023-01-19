c5beba54beae353c48b55cce0e48b0835575c102
Agnes <almoha@gmail.com>
2023-01-19 18:33:19 +0100

    ...

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
-import Modal from "./lib/components/Modal";
+import Modal from "./components/Modal";
+
+export default Modal;
 
 
 
